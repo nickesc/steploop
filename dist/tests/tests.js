f41a5e1c86a555d44814de8889daeabb6e0de412
N. Escobar <nick@nescobar.media>
2025-06-16 05:19:53 -0400

    add dist

---
+++ dist/tests/tests.js
@@ -0,0 +1,50 @@
+import { StepLoop } from "../steploop.js";
+let initial = 0;
+let final = -1;
+function isWithinOne(target, num) {
+    if (target == undefined || num == undefined)
+        return false;
+    return Math.abs(target - num) <= 1;
+}
+class App extends StepLoop {
+    initial() {
+        initial = Date.now();
+        console.log("initial", initial);
+    }
+    async background() {
+        //console.log("bg", this.step_num)
+    }
+    before() {
+        //console.log("before", this.step_num)
+    }
+    step() {
+        //console.log("step ", this.get_step())
+    }
+    after() {
+        //console.log("after", this.step_num)
+    }
+    final() {
+        final = Date.now();
+        let execution_time = (final - initial) / 1000 * this.get_sps();
+        let target_time = this.get_lifespan();
+        console.log("Final", final);
+        console.log(`${execution_time} vs. ${target_time}`);
+        if (!isWithinOne(target_time, execution_time)) {
+            throw new Error("\n\FAILURE: `steploop` lifespan did not match execution time\n\n");
+        }
+        //console.log("final", this.step_num)
+    }
+}
+function testSteploop() {
+    const testLoop = new App(100, 1000);
+    testLoop.start();
+    return true;
+}
+const status = testSteploop();
+if (status) {
+    console.log("\n\nSUCCESS: `steploop` PASSED automated tests\n\n");
+}
+else {
+    throw new Error("\n\FAILURE: `steploop` FAILED automated tests\n\n");
+}
+//(-)/1000*60
